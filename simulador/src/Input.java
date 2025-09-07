e5288795cd65be500668f6e1e08b2b7ff6b06772
pc <matheuss.monteiro9@gmail.com>
2025-09-07 11:50:00 -0300

    a

---
+++ simulador/src/Input.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+
+public class Input {
+
+    private double tamEntradaCano; //Bitola
+    private double volume;
+    private double pressao;
+
+    public static final double MIN_VOLUMEM3 = 0.0; //Valor mínimo de entrada de água em cm^3
+    public static final double MAX_VOLUMEM3 = 100.0; //Valor máximo de entrada de água em cm^3
+
+    public static final double MIN_BITOLA_MM = 10.0; //Tamanho mínimo da bitola em milímetros
+    public static final double MAX_BITOLA_MM = 100.0; //Tamanho máximo da bitola em milímetros
+
+    private Random random = new Random();
+
+
+
+    //pressao = volume/bitola^2
+}
